3d5d2ebeac89f4805a298ad523df81124db9fa74
kizey <ikizey@icloud.com>
2023-01-12 04:40:19 +0400

    use ai route

--- src/server.js
+++ src/server.js
@@ -3,10 +3,14 @@ const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const aiRoutes = require('./api/routes/ai');
+
 dotenv.config({ path: `.env` });
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use('/ai', aiRoutes);
 
 app.get('/', async (req, res, next) => {
   res.send('under development');
