7b3718811006d2de945ed3ad641c43066d06d990
kizey <ikizey@icloud.com>
2023-01-12 20:02:03 +0400

    handle login

--- src/routes/Auth.route.js
+++ src/routes/Auth.route.js
@@ -27,15 +27,35 @@ router.post('/register', async (req, res, next) => {
   }
 });
 
-router.post('/login', (req, res, next) => {
-  res.send('login route');
+router.post('/login', async (req, res, next) => {
+  try {
+    const result = await authSchema.validateAsync(req.body);
+    const user = await User.findOne({ email: result.email });
+
+    if (!user) {
+      throw createError.NotFound('User not registered.');
+    }
+
+    const isMatch = await user.isValidPassword(result.password);
+    if (!isMatch) {
+      throw createError.Unauthorized('Invalid Username/Password.');
+    }
+
+    const accessToken = await signedAccessToken(user.id);
+    res.send({ accessToken });
+  } catch (error) {
+    if (error.isJoi === true) {
+      return next(createError.BadRequest('Invalid Username/Password.'));
+    }
+    next(error);
+  }
 });
 
-router.post('/refresh-token', (req, res, next) => {
+router.post('/refresh-token', async (req, res, next) => {
   res.send('refresh token route');
 });
 
-router.delete('/logout', (req, res, next) => {
+router.delete('/logout', async (req, res, next) => {
   res.send('logout route');
 });
 
