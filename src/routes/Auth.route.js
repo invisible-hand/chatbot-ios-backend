281cb8e2897fb45ada9f4f1a8e4ac7350b5d98c1
kizey <ikizey@icloud.com>
2023-01-12 18:31:29 +0400

    validate email and password

--- src/routes/Auth.route.js
+++ src/routes/Auth.route.js
@@ -2,25 +2,25 @@ const express = require('express');
 const router = express.Router();
 const createError = require('http-errors');
 const User = require('../models/User.model');
+const { authSchema } = require('../utils/validation_schema');
 
 router.post('/register', async (req, res, next) => {
   try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      throw createError.BadRequest();
-    }
+    const result = await authSchema.validateAsync(req.body);
 
-    const doesExist = await User.findOne({ email });
+    const doesExist = await User.findOne({ email: result.email });
     if (doesExist) {
-      throw createError.Conflict(`${email} is already registered`);
+      throw createError.Conflict(`${result.email} is already registered`);
     }
     //TODO! HASH PASSWORD!
-    //TODO! VALIDATE EMAIL!
-    const user = new User({ email, password });
+    const user = new User(result);
     const savedUser = await user.save();
 
     res.send(savedUser);
   } catch (error) {
+    if (error.isJoi === true) {
+      error.status = 422;
+    }
     next(error);
   }
 });
