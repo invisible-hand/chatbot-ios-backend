6cd134e105836698abd69dcba74a4903059ff875
kizey <ikizey@icloud.com>
2023-01-12 20:49:02 +0400

    handle refresh-token route

--- src/routes/Auth.route.js
+++ src/routes/Auth.route.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const createError = require('http-errors');
 const User = require('../models/User.model');
 const { authSchema } = require('../utils/validation_schema');
-const { signAccessToken, signRefreshToken } = require('../utils/jwt_utils');
+const {
+  signAccessToken,
+  signRefreshToken,
+  verifyRefreshToken,
+} = require('../utils/jwt_utils');
 
 router.post('/register', async (req, res, next) => {
   try {
@@ -54,7 +58,20 @@ router.post('/login', async (req, res, next) => {
 });
 
 router.post('/refresh-token', async (req, res, next) => {
-  res.send('refresh token route');
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      throw createError.BadRequest();
+    }
+
+    const userId = await verifyRefreshToken(refreshToken);
+
+    const accessToken = await signAccessToken(userId);
+    const newRefreshToken = await signRefreshToken(userId);
+    res.send({ accessToken, refreshToken: newRefreshToken });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete('/logout', async (req, res, next) => {
