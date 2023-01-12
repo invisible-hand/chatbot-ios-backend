95011c910371cadfc28e2dc56dca149a22431812
kizey <ikizey@icloud.com>
2023-01-12 18:41:48 +0400

    hash password

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -13,6 +14,17 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+});
+
+UserSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = mongoose.model('user', UserSchema);
