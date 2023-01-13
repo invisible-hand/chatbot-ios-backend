f4084e51e6348c87922aade03533fe644115e84e
kizey <ikizey@icloud.com>
2023-01-14 03:50:12 +0400

    add chat model.

---
+++ src/models/chat.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const ChatSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    index: true,
+  },
+  topic_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: mongoose.Types.ObjectId,
+    index: true,
+    required: true,
+  },
+  topic: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  response: {
+    type: String,
+    default: null,
+  },
+  created_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+ChatSchema.statics.deleteByTopicId = function (topicId) {
+  return this.deleteMany({ topic_id: topicId });
+};
+
+const Chat = mongoose.model('Chat', ChatSchema);
+
+module.exports = Chat;
