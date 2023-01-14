11b84fd73796ff448600baa1781eeb5b1a3e5bb8
kizey <ikizey@icloud.com>
2023-01-14 06:11:10 +0400

    rename user to user_id

--- src/models/chat.model.js
+++ src/models/chat.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ChatSchema = new mongoose.Schema({
-  user: {
+  user_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
