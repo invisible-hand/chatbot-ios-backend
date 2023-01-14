2e1140564f19ac4564154beb870807186f90fbda
kizey <ikizey@icloud.com>
2023-01-14 18:54:29 +0400

    use joi-objectid to validate topicid

--- src/utils/validation_schema.js
+++ src/utils/validation_schema.js
@@ -1,4 +1,7 @@
 const Joi = require('joi');
+const JoiObjectId = require('joi-objectid');
+
+Joi.objectId = JoiObjectId(Joi);
 
 const authSchema = Joi.object({
   email: Joi.string().email().lowercase().required(),
@@ -7,7 +10,7 @@ const authSchema = Joi.object({
 
 const messageSchema = Joi.object({
   message: Joi.string().min(1).max(2000).required(),
-  topic_id: Joi.alternatives().try(Joi.string(), Joi.allow(null)).required(),
+  topic_id: Joi.alternatives().try(Joi.objectId(), Joi.allow(null)).required(),
 });
 
 module.exports = { authSchema, messageSchema };
